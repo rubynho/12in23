192bf57486e18e25ccc1248e7b76dfbd901da2a4
rubynho <brunorafael6656@gmail.com>
2023-01-13 09:35:45 -0300

    Improve the lasagna exercise by adding a new constant
    Set oven time per layer as a constant as it's a fixed value.

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -1,6 +1,7 @@
 package lasagna
 
 const OvenTime = 40
+const OvenTimePerLayer = 2
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
 func RemainingOvenTime(actualMinutesInOven int) int {
@@ -9,8 +10,7 @@ func RemainingOvenTime(actualMinutesInOven int) int {
 
 // PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
 func PreparationTime(numberOfLayers int) int {
-	timePerLayer := OvenTime / 20
-	return timePerLayer * numberOfLayers
+	return numberOfLayers * OvenTimePerLayer
 }
 
 // ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
